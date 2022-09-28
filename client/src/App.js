a2b4c25c00e5de974faa02c1759c7884b0727a22
rysaldiwijaya <rysaldii@gmail.com>
2022-09-28 16:28:06 +0700

    improved homepage, added setup for detail page, unfinished but times up, will continue in branch release

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import JobVacancyList from "./components/JobVacancyList";
 import AddNewJob from "./pages/AddNewJob";
+import DetailJobPage from "./pages/DetailJobPage";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Route path="/" element={<HomePage />}>
 				<Route path="" element={<JobVacancyList />} />
 				<Route path="buat-lowongan-pekerjaan" element={<AddNewJob />} />
+				<Route path="detail-lowongan-perkerjaan/:jobVacancyCode" element={<DetailJobPage/>} />
 			</Route>
 		</Routes>
 	);
